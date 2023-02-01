349909993919eb2aeff7259a0adc360ddd62607c
fernando <f.montallana.dit.cvsu@gmail.com>
2023-02-01 16:48:36 +0800

    header changes

--- app/head.js
+++ app/head.js
@@ -6,6 +6,7 @@ export default function Head() {
       <meta name="description" content="Watch anime without disturbing ads." />
       <meta name="" ></meta>
       <meta property="og:title" content="aniwatch" />
+      <meta property="og:description" content="watch disturbing ads without anime. XD" />
       <meta property="og:image" content="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx156067-Jovklss4VWIx.jpg" />
       <meta property="og:type" content="website" />
       <link rel="icon" href="/favicon.ico" />
