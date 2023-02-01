0bfc26df80b5fc38881ae944376d043b206f4c90
fernando <f.montallana.dit.cvsu@gmail.com>
2023-02-01 16:45:16 +0800

    header changes

--- app/head.js
+++ app/head.js
@@ -4,6 +4,10 @@ export default function Head() {
       <title>aniwatch | watch anime without ads</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Watch anime without disturbing ads." />
+      <meta name="" ></meta>
+      <meta property="og:title" content="aniwatch" />
+      <meta property="og:image" content="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx156067-Jovklss4VWIx.jpg" />
+      <meta property="og:type" content="website" />
       <link rel="icon" href="/favicon.ico" />
     </>
   )
