d820e15eeaa056fcb0b0b7b82dbd2c8b2fe3509f
fernando <f.montallana.dit.cvsu@gmail.com>
2023-02-02 10:52:48 +0800

    changes in episode thumbnail

--- app/info/[id]/layout.js
+++ app/info/[id]/layout.js
@@ -5,7 +5,6 @@ export default function InfoLayout({ children }) {
     return (
         <div>
             <Suspense fallback={<InfoLoading />}>
-
                 {children}
             </Suspense>
         </div>
