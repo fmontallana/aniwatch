ab5ba4391101c58ed7c3876e833e18bd7a224f54
fernando <f.montallana.dit.cvsu@gmail.com>
2023-02-03 12:54:40 +0800

    page changed

--- app/watch/[episodeId]/page.js
+++ app/watch/[episodeId]/page.js
@@ -1,10 +1,15 @@
 
 import { PlayerSection } from '@/components';
 import { getAnimeStreamingLinks } from '@/functions/getAnimeFn'
+import { notFound } from 'next/navigation'
 
 export default async function Watch({ params }) {
 
+
     const ep = await getAnimeStreamingLinks(params.episodeId)
+
+    if (ep.message) return notFound()
+
 
     const url = await ep?.sources?.filter((src, index) => {
         return index === 0
