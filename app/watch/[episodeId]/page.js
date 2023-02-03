647a22695eedf57f00a4f29054be73b9e207d326
fernando <f.montallana.dit.cvsu@gmail.com>
2023-02-03 12:37:59 +0800

    fixed link

--- app/watch/[episodeId]/page.js
+++ app/watch/[episodeId]/page.js
@@ -6,10 +6,13 @@ export default async function Watch({ params }) {
 
     const ep = await getAnimeStreamingLinks(params.episodeId)
 
+    const url = ep.sources.filter((src, index) => {
+        return index === 0
+    })
 
     return (
         <div className='h-screen w-full'>
-            <PlayerSection sources={ep.sources} />
+            <PlayerSection src={url[0].url} />
         </div>
     )
 }
--- components/PlayerSection.js
+++ components/PlayerSection.js
@@ -3,17 +3,11 @@
 import HLSPlayer from "./HLSPlayer"
 
 
-export default function PlayerSection({ sources }) {
+export default function PlayerSection({ src }) {
 
-    const url = sources.filter((src, index) => {
-        return index === 0
-    })
-    console.log(url[0])
     return (
         <div className="absolute top-0 left-0 h-[100dvh] w-full">
-            <HLSPlayer
-                src={url[0].url}
-            />
+            <HLSPlayer src={src} />
         </div>
     )
 }
