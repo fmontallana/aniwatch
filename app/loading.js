1b5a06a05dc64b7afabb9363757e319eb04abc8c
fernando <f.montallana.dit.cvsu@gmail.com>
2023-02-07 22:50:12 +0800

    adjusted loaders

--- app/loading.js
+++ app/loading.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Loading() {
     return (
-        <div className="h-screen w-full grid place-items-center text-white text-lg font-bold fs-125">
-            <span className="animate-ping absolute -top-8 z-[1] inline-flex h-24 w-24 rounded-full bg-sky-400 opacity-75">
+        <div className="h-[100dvh] w-full grid place-items-center text-white text-lg font-bold fs-125">
+            <span className="animate-ping inline-flex h-24 w-24 rounded-full bg-sky-400 opacity-75">
             </span>
         </div>
 
