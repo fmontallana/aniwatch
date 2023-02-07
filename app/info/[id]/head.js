16a6fe919320b80ac57f22b05ab82428ac374955
fernando <f.montallana.dit.cvsu@gmail.com>
2023-02-07 23:57:31 +0800

    changes in page head

--- app/info/[id]/head.js
+++ app/info/[id]/head.js
@@ -3,15 +3,16 @@ import { useGlobalStore } from "@/store/globalStore";
 
 export default async function Head({ params }) {
 
-    const { title, description, image } = await useAnimeStore.getState().fetchAnimeInfo(params.id);
+    const { title, description, cover } = await useAnimeStore.getState().fetchAnimeInfo(params.id);
     const filteredTitle = useGlobalStore.getState().filterTitleLanguage(title)
+
     return (
         <>
             <title>{filteredTitle}</title>
-            <meta name="description" content={"Watch anime without ads"} />
+            <meta name="description" content={description.substring(0, 199)} />
             <meta property="og:title" content={filteredTitle} />
-            <meta property="og:description" content={"Watch anime without ads"} />
-            <meta property="og:image" content={image || "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx156067-Jovklss4VWIx.jpg"} />
+            <meta property="og:description" content={description.substring(0, 199)} />
+            <meta property="og:image" content={cover || "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx156067-Jovklss4VWIx.jpg"} />
             <meta property="og:type" content="website" />
         </>
     );
